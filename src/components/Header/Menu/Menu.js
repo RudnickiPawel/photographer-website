6f54b85a721b2a6d96ca317cc59ae2a2d7eaa360
RudnickiPawel <rudnicki.pawel94@gmail.com>
2021-10-08 15:31:40 +0200

    bg image changes, menu on mobile initially hidden

--- src/components/Header/Menu/Menu.js
+++ src/components/Header/Menu/Menu.js
@@ -2,7 +2,7 @@ import '../../../main.css';
 
 const Menu = () => {
   return (
-    <div className="Menu">
+    <div className="Menu Menu--hidden">
       <div className="Menu__link">Portfolio</div>
       <div className="Menu__link">About</div>
       <div className="Menu__link">Contact</div>
